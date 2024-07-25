cf86c4be17603dcef8dbde7c81f28dfbdedc8564
rochi88 <raisul.me@gmail.com>
2024-07-25 16:18:16 +0600

    wip

---
+++ 02_variables/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Raisul"
+	const days int = 7
+	var x, y int = 1, 2
+	var z uint
+	temp := 1
+
+	fmt.Println("Checking the uses of variables")
+	fmt.Printf("Name is %v, Days is %v, x is %v, y is %v, z is %v, temp is %v\n", name, days, x, y, z, temp)
+}
